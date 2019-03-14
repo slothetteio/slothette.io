9410f1e74faf9ef072b796bcf63124c35d6273c4
slothette <slothette.io@gmail.com>
2019-03-14 21:52:51 +0000

    remove scroll management for dev env

--- src/client.jsx
+++ src/client.jsx
@@ -5,4 +5,9 @@ import { ScrollContext } from 'react-router-scroll-4';
 
 import App from 'app';
 
-ReactDOM.render(<Router><ScrollContext><App /></ScrollContext></Router>, window.document.getElementById('app'));
+if (process.env.NODE_ENV === 'production') {
+  ReactDOM.render(<Router><ScrollContext><App/></ScrollContext></Router>, window.document.getElementById('app'));
+} else {
+  // don't need the scroll fix in development since I refresh often and it keeps scrolling up
+  ReactDOM.render(<Router><App/></Router>, window.document.getElementById('app'));
+}

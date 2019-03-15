a003512f3bbdb9ba8f5660f73a54d8716d1b7963
slothette <slothette.io@gmail.com>
2019-03-15 18:41:17 +0000

    ref: wording

--- src/utils/render-count/index.jsx
+++ src/utils/render-count/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { idFor } from '../../helpers/utils';
 
 function RenderCount() {
-  let [s] = React.useState({f: idFor('')});
+  let [count] = React.useState({f: idFor('')});
   return (
     <>
-      (r:{s.f()})
+      (r:{count.f()})
     </>
   );
 }
